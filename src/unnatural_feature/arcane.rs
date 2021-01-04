4da81ecb488501e07641f7eb23187c7e30bc0bbc
Alexander Gräfe <alexander.graefe@googlemail.com>
2021-01-04 14:35:14 +0100

    More type shenanigans

--- src/unnatural_feature/arcane.rs
+++ src/unnatural_feature/arcane.rs
@@ -1,7 +1,15 @@
 use std::fmt::Display;
 
 use crate::{dice::Rollable, Alignment};
-pub struct Arcane(String, Alignment);
+
+#[derive(Debug, PartialEq)]
+pub enum Arcane {
+    Residue(Alignment),
+    Blight(Alignment),
+    Alteration(Alignment),
+    Enchantment(Alignment),
+    Source(Alignment),
+}
 
 impl Arcane {
     pub fn new<T: ?Sized>(dice: &mut T) -> Arcane
@@ -10,11 +18,11 @@ impl Arcane {
     {
         let alignment = Alignment::new(dice);
         match dice.roll() {
-            1..=2 => Arcane(String::from("residue"), alignment),
-            3..=5 => Arcane(String::from("blight"), alignment),
-            6..=7 => Arcane(String::from("alteration/mutation"), alignment),
-            8..=10 => Arcane(String::from("enchantment"), alignment),
-            11..=12 => Arcane(String::from("source/repository"), alignment),
+            1..=2 => Arcane::Residue(alignment),
+            3..=5 => Arcane::Blight(alignment),
+            6..=7 => Arcane::Alteration(alignment),
+            8..=10 => Arcane::Enchantment(alignment),
+            11..=12 => Arcane::Source(alignment),
             _ => unreachable!(),
         }
     }
@@ -22,12 +30,21 @@ impl Arcane {
 
 impl Display for Arcane {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}\nAlignment: {}", self.0, self.1)
+        match self {
+            Arcane::Residue(alignment) => write!(f, "residue\nAlignment: {}", alignment),
+            Arcane::Blight(alignment) => write!(f, "blight\nAlignment: {}", alignment),
+            Arcane::Alteration(alignment) => {
+                write!(f, "alteration/mutation\nAlignment: {}", alignment)
+            }
+            Arcane::Enchantment(alignment) => write!(f, "enchantment\nAlignment: {}", alignment),
+            Arcane::Source(alignment) => write!(f, "source/repository\nAlignment: {}", alignment),
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::alignment::*;
     use crate::dice::*;
 
     use super::Arcane;
@@ -36,8 +53,7 @@ mod tests {
     fn it_works() {
         let mut dice = MockDice::new(vec![1]);
         let arcane = Arcane::new(&mut dice);
-        assert_eq!(arcane.0, String::from("residue"));
-        assert_eq!(arcane.1.to_string(), String::from("Chaotic"))
+        assert_matches!(arcane, Arcane::Residue(alignment) if alignment == Alignment::Chaotic)
     }
 
     #[test]
--- src/unnatural_feature/planar.rs
+++ src/unnatural_feature/planar.rs
@@ -1,8 +1,14 @@
 use std::fmt::Display;
 
-use crate::{dice::Rollable, Alignment};
+use crate::{dice::Rollable, Alignment, Element};
 
-pub struct Planar(String, Alignment);
+#[derive(Debug, PartialEq)]
+pub enum Planar {
+    Distortion(Alignment, Element),
+    Portal(Alignment, Element),
+    Rift(Alignment, Element),
+    Outpost(Alignment, Element),
+}
 
 impl Planar {
     pub fn new<T: ?Sized>(dice: &mut T) -> Planar
@@ -10,11 +16,12 @@ impl Planar {
         T: Rollable,
     {
         let alignment = Alignment::new(dice);
+        let element = Element::new(dice);
         match dice.roll() {
-            1..=4 => Planar(String::from("distortion/warp"), alignment),
-            5..=8 => Planar(String::from("portal/gate"), alignment),
-            9..=10 => Planar(String::from("rift/tear"), alignment),
-            11..=12 => Planar(String::from("outpost"), alignment),
+            1..=4 => Planar::Distortion(alignment, element),
+            5..=8 => Planar::Portal(alignment, element),
+            9..=10 => Planar::Rift(alignment, element),
+            11..=12 => Planar::Outpost(alignment, element),
             _ => unreachable!(),
         }
     }
@@ -22,22 +29,42 @@ impl Planar {
 
 impl Display for Planar {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}\nAlignment: {}", self.0, self.1)
+        match self {
+            Planar::Distortion(alignment, element) => write!(
+                f,
+                "distortion/warp\nAlignment: {}\nElement: {}",
+                alignment, element
+            ),
+            Planar::Portal(alignment, element) => write!(
+                f,
+                "portal/gate\nAlignment: {}\nElement: {}",
+                alignment, element
+            ),
+            Planar::Rift(alignment, element) => write!(
+                f,
+                "rift/tear\nAlignment: {}\nElement: {}",
+                alignment, element
+            ),
+            Planar::Outpost(alignment, element) => {
+                write!(f, "outpost\nAlignment: {}\nElement: {}", alignment, element)
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::alignment::*;
     use crate::dice::*;
+    use crate::element::*;
 
     use super::Planar;
 
     #[test]
     fn it_works() {
-        let mut dice = MockDice::new(vec![3, 1]);
+        let mut dice = MockDice::new(vec![1, 11, 3]);
         let planar = Planar::new(&mut dice);
-        assert_eq!(planar.0, String::from("distortion/warp"));
-        assert_eq!(planar.1.to_string(), String::from("Evil"))
+        assert_matches!(planar, Planar::Distortion(alignment, element) if alignment == Alignment::Chaotic && element == Element::Death)
     }
 
     #[test]
@@ -46,7 +73,7 @@ mod tests {
         let arcane = Planar::new(&mut dice);
         assert_eq!(
             arcane.to_string(),
-            String::from("distortion/warp\nAlignment: Evil")
+            String::from("distortion/warp\nAlignment: Evil\nElement: air")
         );
     }
 }
